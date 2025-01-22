546b85b25cda7bf138032db93d2bee5ef4260127
aniketamte <aniketamte123@gmail.com>
2025-01-22 19:03:25 +0530

    Fibonacci Number using DP

--- fibo.java
+++ fibo.java
@@ -10,9 +10,21 @@ public class fibo {
           dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
           return dp[n];
       }
+
+      //Tabulataion Form
+      public static int fibTabulation(int n){
+            int dp[] = new int[n+1];
+            dp[0]=0;
+            dp[1]=1;
+            for(int i=2; i<=n; i++){
+                  dp[i]=dp[i-1]+dp[i-2];
+            }
+            return dp[n]; //ans;
+      }
       public static void main(String[] args) {
             int n=5;
             int dp[] = new int[n+1];
             System.out.println(fibo(n, dp));
+            System.out.println(fibTabulation(n));
       }
 }
