3594beb22fb8064a5f22b5d607aff6370f2a2696
aniketamte <aniketamte123@gmail.com>
2025-01-23 19:35:14 +0530

    01 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -1,62 +1,64 @@
+
 public class KnapsackProblem01 {
-      //This is Recursion code
-      public static int knapsanck(int val[], int wt[], int W, int n){
-          //W = Capacity of Bag  
-          if(W == 0 || n == 0){
+    //This is Recursion code
+
+    public static int knapsanck(int val[], int wt[], int W, int n) {
+        //W = Capacity of Bag  
+        if (W == 0 || n == 0) {
             return 0;
-          }
-          if(wt[n-1] <= W){  //Valid condition
+        }
+        if (wt[n - 1] <= W) {  //Valid condition
             //include
 
-            int ans1= val[n-1] + knapsanck(val, wt, W-wt[n-1], n-1);
+            int ans1 = val[n - 1] + knapsanck(val, wt, W - wt[n - 1], n - 1);
 
             //exlude
-
-            int ans2 = knapsanck(val, wt, W, n-1);
+            int ans2 = knapsanck(val, wt, W, n - 1);
             return Math.max(ans1, ans2);
-          } else{
+        } else {
             //Not valid Condition
             return knapsanck(val, wt, W, n - 1);
-          }
-      }
+        }
+    }
 
-      //Memorization Code
-
-      public static int knapsanckMemorization(int val[], int wt[], int W, int n, int dp[][]){
-            //W = Capacity of Bag  
-          if (W == 0 || n == 0) {
-              return 0;
-          }
-          if(dp[n][W] != -1){
+    //Memorization Code
+    public static int knapsanckMemorization(int val[], int wt[], int W, int n, int dp[][]) {
+        //W = Capacity of Bag  
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+        if (dp[n][W] != -1) {
             return dp[n][W];
-          }
-          if (wt[n - 1] <= W) {  //Valid condition
-              //include
+        }
+        if (wt[n - 1] <= W) {  //Valid condition
+            //include
 
-              int ans1 = val[n - 1] + knapsanckMemorization(val, wt, W - wt[n - 1], n - 1, dp);
+            int ans1 = val[n - 1] + knapsanckMemorization(val, wt, W - wt[n - 1], n - 1, dp);
 
-              //exlude
-              int ans2 = knapsanckMemorization(val, wt, W, n - 1, dp);
-              dp[n][W] = Math.max(ans1, ans2);
-              return dp[n][W];
-          } else {
-              //Not valid Condition
-              dp[n][W] = knapsanckMemorization(val, wt, W, n - 1, dp);
-              return dp[n][W];
-      }
-}
-      public static void main(String[] args) {
-            int val[] = {15, 14, 10, 45, 30};
-            int wt[] = {2, 5, 1, 3, 4};
-            int W = 7;
-            int dp[][] = new int[val.length+1][W+1];
+            //exlude
+            int ans2 = knapsanckMemorization(val, wt, W, n - 1, dp);
+            dp[n][W] = Math.max(ans1, ans2);
+            return dp[n][W];
+        } else {
+            //Not valid Condition
+            dp[n][W] = knapsanckMemorization(val, wt, W, n - 1, dp);
+            return dp[n][W];
+        }
+    }
 
-            //Initialization
-            for(int i=0; i<dp.length; i++){
-                  for(int j=0; j<dp[0].length; j++){
-                        dp[i][j] = -1;
-                  }
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+        int dp[][] = new int[val.length + 1][W + 1];
+
+        //Initialization
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
             }
-            System.out.println(knapsanck(val, wt, W, val.length));
-      }
+        }
+        System.out.println(knapsanck(val, wt, W, val.length));
+        System.out.println(knapsanckMemorization(val, wt, W, val.length, dp));
+    }
 }
