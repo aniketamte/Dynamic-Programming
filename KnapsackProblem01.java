5c2ec74cfad7f1b637e86d1f0ad2de19a0d27870
aniketamte <aniketamte123@gmail.com>
2025-01-23 18:58:53 +0530

    01 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -22,5 +22,6 @@ public class KnapsackProblem01 {
             int val[] = {15, 14, 10, 45, 30};
             int wt[] = {2, 5, 1, 3, 4};
             int W = 7;
+            System.out.println(knapsanck(val, wt, W, val.length));
       }
 }
