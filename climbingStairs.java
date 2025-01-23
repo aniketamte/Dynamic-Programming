a9e6e3f2c51ca6484dc541a3c75fe017f9c478af
aniketamte <aniketamte123@gmail.com>
2025-01-23 17:54:21 +0530

    Climbing Stairs using Memorization in DP

--- climbingStairs.java
+++ climbingStairs.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class climbingStairs {
       //Leetcode 70
       
@@ -12,8 +15,27 @@ public class climbingStairs {
             }
             return countWays(n-1) + countWays(n-2);
       }
+
+      //Memorization ---> O(n)
+      public static int countWaysMemorization(int n, int ways[]) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+        ways[n] = countWaysMemorization(n - 1, ways) + countWaysMemorization(n - 2, ways);
+        return ways[n];
+    }
+
       public static void main(String[] args) {
             int n=5;
+            int ways[] = new int[n+1];
+            Arrays.fill(ways, -1);
             System.out.println(countWays(n));
+            System.out.println(countWaysMemorization(n, ways));
       }
 }
