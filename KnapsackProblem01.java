22eeb5e255d026b13ff7ff4e3cad48a7d4e10729
aniketamte <aniketamte123@gmail.com>
2025-01-24 22:20:12 +0530

    0-1 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -76,12 +76,18 @@ public class KnapsackProblem01 {
                 }
             }
         }
-        print[dp];
+        print(dp);
         return dp[n][W];
     }
 
     public static void print(int dp[][]){
-        
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                System.out.print(dp[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
