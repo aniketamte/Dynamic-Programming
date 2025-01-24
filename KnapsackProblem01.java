d250ed825772f746e28306c47eda6d1c8a2737db
aniketamte <aniketamte123@gmail.com>
2025-01-24 22:21:10 +0530

    0-1 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -47,31 +47,30 @@ public class KnapsackProblem01 {
     }
 
     //Tabular Method Code for 0-1 Knapsack
-
-    public static int knapsackTab(int val[], int wt[], int W){
+    public static int knapsackTab(int val[], int wt[], int W) {
         int n = val.length;
-        int dp[][] = new int[n+1][W+1];
-        for(int i=0; i<dp.length; i++){
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
             //for 0th column
             dp[i][0] = 0;
         }
-        for(int j=0; j<dp[0].length; j++){
+        for (int j = 0; j < dp[0].length; j++) {
             //for 0th row
             dp[0][j] = 0;
         }
 
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<W+1; j++){
-                int v= val[i-1]; //ith item val
-                int w = wt[i-1];  //ith item wait
-                if(w <= j){
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1]; //ith item val
+                int w = wt[i - 1];  //ith item wait
+                if (w <= j) {
                     //for valid condition
-                    int incProfit = v + dp[i-1][j-w];
+                    int incProfit = v + dp[i - 1][j - w];
                     int excProfit = dp[i - 1][j];
                     dp[i][j] = Math.max(incProfit, excProfit);
-                } else{
+                } else {
                     //invalid condition
-                    int excProfit = dp[i-1][j];
+                    int excProfit = dp[i - 1][j];
                     dp[i][j] = excProfit;
                 }
             }
@@ -80,9 +79,9 @@ public class KnapsackProblem01 {
         return dp[n][W];
     }
 
-    public static void print(int dp[][]){
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
                 System.out.print(dp[i][j] + " ");
             }
             System.out.println();
