497babd0fb08a3e35c076b21415b47a88c790a1a
aniketamte <aniketamte123@gmail.com>
2025-01-25 11:45:19 +0530

    Target Sum Tabulation Method ---> DP

--- TargetSum.java
+++ TargetSum.java
@@ -12,9 +12,16 @@ public class TargetSum {
                   for(int j=1; j<sum+1; j++){
                         int v = arr[i-1];
                         //include
+                        if(v<=j && dp[i-1][j-v]  == true){
+                              dp[i][j] = true;
+                        }
                         //exclude
+                        else if(dp[i - 1][j] == true){
+                              dp[i][j]=true;
+                        }
                   }
             }
+            return dp[n][sum];
       }
       public static void main(String[] args) {
             int arr[] = {4,2,7,1,3};
