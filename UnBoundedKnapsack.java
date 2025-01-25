aaa3d1310bcc4e90828bc9113486f2155657b2ac
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:29:27 +0530

    Unbounded Knapsack

--- UnBoundedKnapsack.java
+++ UnBoundedKnapsack.java
@@ -25,7 +25,18 @@ public class UnBoundedKnapsack {
                 }
             }
         }
+        print(dp);
         return dp[n][W];
+    }
+
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
