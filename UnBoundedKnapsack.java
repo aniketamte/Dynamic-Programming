580eb88718b88dae4a4448bc843bb3440e8f2d0c
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:19:30 +0530

    Unbounded Knapsack

--- UnBoundedKnapsack.java
+++ UnBoundedKnapsack.java
@@ -1,6 +1,9 @@
 
 public class UnBoundedKnapsack {
-
+      public static int unboundedKnap(int val[], int wt[], int W){
+            int n = val.length;
+          int dp[][] = new int[n + 1][W + 1];
+      }
     public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
