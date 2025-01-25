084fada1ddf615ffc03acca753531250cc145611
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:17:13 +0530

    Unbounded Knapsack

---
+++ UnBoundedKnapsack.java
@@ -0,0 +1,7 @@
+public class UnBoundedKnapsack {
+      public static void main(String[] args) {
+            int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+      }
+}
