ea80c4be7063038e1f863855218d8dad37cc8389
aniketamte <aniketamte123@gmail.com>
2025-01-25 22:03:46 +0530

    Coin Change Using DP

--- coinChange.java
+++ coinChange.java
@@ -1,7 +1,11 @@
 public class coinChange {
+      public static int coinChanges(int coins[], int sum){
+            int n = coins.length;
+            int dp[][] = new int[n+1][sum+1];
+      }
       public static void main(String[] args) {
             int coins[] = {1,2,3};
             int sum = 4;
-            
+
       }
 }
