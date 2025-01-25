ab0f2f8ddd34ce2d1c7f8f2896b6d475b0494439
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:27:13 +0530

    Unbounded Knapsack

--- UnBoundedKnapsack.java
+++ UnBoundedKnapsack.java
@@ -13,11 +13,24 @@ public class UnBoundedKnapsack {
             //for 0th row
             dp[0][j] = 0;
         }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                if(wt[i-1] <= j){
+                  //valid
+                  dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                } else{
+                  //invalid
+                  dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
     }
 
     public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
         int W = 7;
+        System.out.println(unboundedKnap(val, wt, W));
     }
 }
