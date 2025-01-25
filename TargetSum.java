7fdb8ab5e9d7f1ac8cc936348a98978530f299c1
aniketamte <aniketamte123@gmail.com>
2025-01-25 11:49:21 +0530

    Target Sum Tabulation Method ---> DP

--- TargetSum.java
+++ TargetSum.java
@@ -23,12 +23,23 @@ public class TargetSum {
                 }
             }
         }
+        print(dp);
         return dp[n][sum];
+    }
+
+    public static void print(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int arr[] = {4, 2, 7, 1, 3};
-        int sum = 20;
+        int sum = 10;
         System.out.println(targetSumSubset(arr, sum));
     }
 }
