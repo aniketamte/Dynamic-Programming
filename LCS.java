3a09f9d166f88d12d24f750d08ea50410815009e
aniketamte <aniketamte123@gmail.com>
2025-01-26 18:05:45 +0530

    Longest Common Subsequence using DP

--- LCS.java
+++ LCS.java
@@ -7,7 +7,7 @@ public class LCS {
        * s2=>length=>m
      */
     //Print
-    public static void print(boolean dp[][]) {
+    public static void print(int dp[][]) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
                 System.out.print(dp[i][j] + " ");
@@ -39,7 +39,7 @@ public class LCS {
         }
 
         //overlaping ans calculated
-        if(dp[n][m] != -1){
+        if (dp[n][m] != -1) {
             return dp[n][m];
         }
 
@@ -52,11 +52,45 @@ public class LCS {
         }
     }
 
+    //Tabulation
+    public static int lcsTabulation(String s1, String s2, int dp[][]) {
+        int n = s1.length();
+        int m = s2.length();
+
+        
+
+        //Initialization for 0th row and 0th column
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        //Tabulation code
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    //different
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        print(dp);
+        return dp[n][m];
+    }
+
     public static void main(String[] args) {
         String s1 = "ABCD";  //"abcdge";
         String s2 = "ACEB"; //"abedg";
         int n = s1.length();
         int m = s2.length();
+        //create 2D array
         int dp[][] = new int[n + 1][m + 1];
 
 
@@ -64,7 +98,6 @@ public class LCS {
          * first initialie whole 2D matrix with value -1
          * and then solve
          */
-
         for (int i = 0; i < n + 1; i++) {
             for (int j = 0; j < m + 1; j++) {
                 dp[i][j] = -1;
@@ -73,5 +106,6 @@ public class LCS {
 
         System.out.println("Recursion Ans : " + lcs(s1, s2, s1.length(), s2.length()));
         System.out.println("Memorization Ans : " + lcsMemorization(s1, s2, n, m, dp));
+        System.out.println("Tabulation Method : " + lcsTabulation(s1, s2, dp));
     }
 }
