edcc863df8d82498de94cf0a015cf0f32e90ece7
aniketamte <aniketamte123@gmail.com>
2025-01-27 09:13:48 +0530

    Longest common Substring using DP

--- LCS2.java
+++ LCS2.java
@@ -33,7 +33,19 @@ public class LCS2 {
                 }
             }
         }
+        print(dp);
         return ans;
+    }
+
+    //print table
+    public static void print(int dp[][]){
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
