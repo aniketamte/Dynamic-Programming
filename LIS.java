df5d0fe34fccc49b7611d0c3d9216700f3e887af
aniketamte <aniketamte123@gmail.com>
2025-01-27 09:32:13 +0530

    Longest increasing subsequence using DP

--- LIS.java
+++ LIS.java
@@ -57,7 +57,19 @@ public class LIS {
                 }
             }
         }
+        print(dp);
         return dp[n][m];
+    }
+
+    //print table
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
