7f74955121ffeaebc97f4a1d9a234fb37bbb7668
aniketamte <aniketamte123@gmail.com>
2025-01-28 18:04:20 +0530

    Matrix Chain Multiplication Using DP

--- MCM.java
+++ MCM.java
@@ -1,25 +1,29 @@
-public class MCM{
-      //Matrix chain multiplication
-      public static int mcm(int arr[], int i, int j){
-            if(i==j){
-                  //Single matrix case
-                  return 0;
-            }
-            int ans = Integer.MAX_VALUE;  //positive Infinity
 
-            for(int k=1; k<=j-1; k++){
-                     int cost1 = mcm(arr, i, k);  // Ai....Ak => arr[i-1] * arr[k]
-                     int cost2 = mcm(arr, i+1, j); //Ai+1....Aj => arr[k] * arr[j]
-                     int cost3 = arr[i-1]*arr[k]*arr[j];
+public class MCM {
+    //Matrix chain multiplication
 
-                     int finalCost = cost1 + cost2 + cost3;
-                     ans = Math.min(ans, finalCost);
-            }
-            return ans;
-      }
-      public static void main(String[] args) {
-            int arr[] = {1,2,3,4,3};
-            //n=5
-            int n = arr.length;
-      }
+    public static int mcm(int arr[], int i, int j) {
+        if (i == j) {
+            //Single matrix case
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;  //positive Infinity
+
+        for (int k = 1; k <= j - 1; k++) {
+            int cost1 = mcm(arr, i, k);  // Ai....Ak => arr[i-1] * arr[k]
+            int cost2 = mcm(arr, i + 1, j); //Ai+1....Aj => arr[k] * arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return ans;   //minCost
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 3};
+        //n=5
+        int n = arr.length;
+        System.out.println(mcm(arr, 1, n - 1));
+    }
 }
