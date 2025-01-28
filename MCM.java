f470759bfaa195d014f6ce212580ae935a3179ba
aniketamte <aniketamte123@gmail.com>
2025-01-28 18:03:16 +0530

    Matrix Chain Multiplication Using DP

--- MCM.java
+++ MCM.java
@@ -11,7 +11,11 @@ public class MCM{
                      int cost1 = mcm(arr, i, k);  // Ai....Ak => arr[i-1] * arr[k]
                      int cost2 = mcm(arr, i+1, j); //Ai+1....Aj => arr[k] * arr[j]
                      int cost3 = arr[i-1]*arr[k]*arr[j];
+
+                     int finalCost = cost1 + cost2 + cost3;
+                     ans = Math.min(ans, finalCost);
             }
+            return ans;
       }
       public static void main(String[] args) {
             int arr[] = {1,2,3,4,3};
