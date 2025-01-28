4e70f8877f04799c0342c610f45b8f38e304486e
aniketamte <aniketamte123@gmail.com>
2025-01-28 17:55:37 +0530

    Matrix Chain Multiplication Using DP

--- MCM.java
+++ MCM.java
@@ -1,6 +1,11 @@
 public class MCM{
       //Matrix chain multiplication
-      public static void main(String[] args) {
+      public static int mcm(int arr[], int i, int j){
             
+      }
+      public static void main(String[] args) {
+            int arr[] = {1,2,3,4,3};
+            //n=5
+            int n = arr.length;
       }
 }
